9bd11142d342cebc89d9e80e671e852a0844dc84
Fabrice Theytaz <2198753+fabricetheytaz@users.noreply.github.com>
2026-01-24 16:15:33 +0100

    JS TS

---
+++ javascript/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+tsc
+
+# uglifyjs js/cdpi.js > js/cdpi.min.js
+# sed -f commands.sed js/cdpi.min.js > js/cdpi2.min.js
+# cat js/geometry.min2.mjs js/dom.min2.mjs js/drawing.min2.mjs js/game.min2.mjs src/export.txt > dist/cdpi.min.mjs
