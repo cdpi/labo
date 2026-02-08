beddf8b617ad029a400ed496a7da8a4dc6354564
Fabrice Theytaz <2198753+fabricetheytaz@users.noreply.github.com>
2026-02-09 00:23:06 +0100

    Rien (dossier led a supprimer)

---
+++ javascript/src/util/pixel.ts
@@ -0,0 +1,15 @@
+
+import { Color } from "../color/color.js";
+
+class Pixel extends Color
+	{
+	constructor(red:number, green:number, blue:number)
+		{
+		super(red, green, blue);
+		}
+	}
+
+export
+	{
+	Pixel
+	};
